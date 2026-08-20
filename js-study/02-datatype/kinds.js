170d13aa3a106bed6fed7ce94045351b8ed0bbd7
soongu <naong526@naver.com>
2026-08-20 14:32:23 +0900

    js-lab 및 js-study 디렉토리에 여러 JavaScript 파일 추가

---
+++ js-study/02-datatype/kinds.js
@@ -0,0 +1,28 @@
+const followerCount = 340;
+const rating = 4.5;
+const temperature = -3;
+
+console.log(followerCount, typeof followerCount);
+console.log(rating, typeof rating);
+console.log(temperature, typeof temperature);
+
+const username = 'hong_tutor';
+const caption = '오늘도 한 장';
+const zipCode = `06236`;
+
+console.log(username, typeof username);
+console.log(caption, typeof caption);
+console.log(zipCode, typeof zipCode);
+
+const isPublic = true;
+const isDeleted = false;
+
+console.log(isPublic, typeof isPublic);
+console.log(isDeleted, typeof isDeleted);
+
+let caption2;
+const deletedAt = null;
+
+console.log(caption2, typeof caption2);
+console.log(deletedAt, typeof deletedAt);
+
---
+++ js-study/02-datatype/type.js
@@ -0,0 +1,4 @@
+let a = 340;
+let b = '340';
+console.log(typeof a);
+console.log(typeof b);
