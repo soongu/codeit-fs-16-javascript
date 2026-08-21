a5ff3b2af9b71ae9787eaf85f643fa85fa33fa29
soongu <naong526@naver.com>
2026-08-21 16:03:16 +0900

    js-lab 및 js-study 디렉토리에 여러 JavaScript 파일 추가

---
+++ js-lab/07-loop/basic-practice.js
@@ -0,0 +1,54 @@
+let sum = 0;
+
+for (let i = 1; i <= 5; i++) {
+  sum = sum + i;
+  console.log(sum);
+}
+
+
+/*
+let count = 0;
+
+for (let i = 1; i <= 20; i++) {
+  count++;
+}
+
+console.log(count);
+
+
+let result = 1;
+
+for (let i = 1; i <= 4; i++) {
+  result = result * i;
+}
+
+console.log(result);
+
+
+let a = 0;
+let b = 1;
+
+for (let i = 1; i <= 3; i++) {
+  let temp = a;
+  a = b;
+  b = temp + b;
+  console.log(a, b);
+}
+
+
+let x = 10;
+
+for (let i = 1; i <= 5; i++) {
+  x = x - i;
+  console.log(x);
+}
+
+
+
+let total = 0;
+
+for (let i = 10; i > 0; i -= 3) {
+  total += i;
+  console.log(i, total);
+}
+*/
---
+++ js-study/07-loop/gugudan.js
@@ -0,0 +1,6 @@
+
+const level = 4; // 단수
+
+for (let n = 1; n <= 9; n++) {
+  console.log(level, 'x', n, '=', level * n);
+}
