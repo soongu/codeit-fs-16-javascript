029ba038aeb0d1f1b46303cce475fdc54e9913b3
soongu <naong526@naver.com>
2026-08-25 09:50:14 +0900

    새로운 JavaScript 파일 추가: 게시물 좋아요 수 집계 및 주간 좋아요 계산 기능 구현

---
+++ js-study/11-array/push-const.js
@@ -0,0 +1,11 @@
+// ~/js-study/push-const.js
+const likeCounts = [];
+
+console.log(likeCounts);
+console.log(likeCounts.length + '개');
+
+likeCounts.push(1240);
+likeCounts.push(0, 500, 900, 1200);
+
+console.log(likeCounts);
+console.log(likeCounts.length + '개');
