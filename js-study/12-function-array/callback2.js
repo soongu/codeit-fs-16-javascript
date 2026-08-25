1b0eee5df47b399e5760ff4b2aaf965ee41c2165
soongu <naong526@naver.com>
2026-08-25 11:33:40 +0900

    새로운 JavaScript 파일 추가: 짝수 필터링 기능 구현

--- js-study/12-function-array/callback2.js
+++ js-study/12-function-array/callback2.js
@@ -14,4 +14,21 @@ simpleFor(foodList, (food) => {
 });
 
 
+console.log('==============');
+// 이 배열에서 짝수만 들어있는 새 배열을 가지고 싶어 (원본훼손 금지)
+function filterEvenArray(numbers) {
+  const newArray = [];
+  for (const n of numbers) {
+    if (n % 2 === 0) {
+      newArray.push(n);
+    }
+  }
+  return newArray;
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = filterEvenArray(numbers);
+console.log(evenNumbers);
+console.log(numbers);
 
