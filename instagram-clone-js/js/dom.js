1ae49c0fc4be8ffccea61ccf5f2becfa36dd620b
soongu <naong526@naver.com>
2026-08-27 09:43:26 +0900

    dom.js에 캡션 관련 코드 추가 및 homeMenu 정보 출력 개선

--- instagram-clone-js/js/dom.js
+++ instagram-clone-js/js/dom.js
@@ -8,3 +8,26 @@ console.log('작성자:', username.textContent);
 caption.textContent = '새벽 러닝 🌙';
 username.textContent = 'soongu';
 
+// step2
+
+const homeMenu = document.querySelector('.nav-menu li');
+console.log(homeMenu.textContent);
+console.log(homeMenu.innerHTML);
+
+caption.textContent = '<b>굵게</b>';
+console.log('글자로 넣으면:', caption.innerHTML);
+
+caption.innerHTML = '<b>굵게</b>';
+console.log('태그로 넣으면:', caption.textContent);
+
+console.log('자식 태그:', caption.firstChild.nodeName);
+
+// step3
+const comment =
+  '<img src="없는파일" onerror="console.log(\'남의 코드가 돌았다\')">';
+
+// caption.innerHTML = comment;
+
+// step4
+const photo = document.querySelector('figure img');
+console.log(photo.attributes);
