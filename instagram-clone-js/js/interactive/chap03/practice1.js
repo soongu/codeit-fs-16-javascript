ae3a5ef8e8621eb82c439c18b170bacf107e1fda
soongu <naong526@naver.com>
2026-08-27 11:49:55 +0900

    practice1.js 파일 추가 및 통계와 게시물 수 출력 기능 구현

---
+++ instagram-clone-js/js/interactive/chap03/practice1.js
@@ -0,0 +1,26 @@
+
+const [firstSect, secondSect]
+  = [...document.querySelectorAll('section')];
+
+const likeListItem = document.createElement('li');
+likeListItem.textContent = '좋아요 4,120';
+
+const statisticsUl = firstSect.querySelector('ul');
+statisticsUl.append(likeListItem);
+
+const [postLi, followerLi, followingLi, likeLi]
+  = [...statisticsUl.children];
+
+console.log(`통계 ${statisticsUl.children.length}줄: ${postLi.textContent} · ${followerLi.textContent} · ${followingLi.textContent} · ${likeLi.textContent}`);
+
+const imageSectionUl = secondSect.querySelector('ul');
+
+// 지우기 전 게시물 수
+const beforePostCount = imageSectionUl.children.length;
+
+imageSectionUl.children[0].remove();
+
+const afterPostCount = imageSectionUl.children.length;
+
+console.log(`게시물 ${beforePostCount}장 -> ${afterPostCount}장`);
+
