9940a10ba58a86f8a87d76af235363021071cce6
soongu <naong526@naver.com>
2026-08-27 09:01:41 +0900

    dom.js에서 코드 정리 및 캡션과 작성자 정보 출력 개선

--- instagram-clone-js/js/dom.js
+++ instagram-clone-js/js/dom.js
@@ -1,11 +1,10 @@
-
-
 // instagram-clone-js/js/dom.js
-const caption = document.querySelector("figcaption");
-console.log("캡션 글자:", caption.textContent);
+const caption = document.querySelector('figcaption');
+console.log('캡션 글자:', caption.textContent);
 
-const username = document.querySelector(".post-header p");
-console.log("작성자:", username.textContent);
+const username = document.querySelector('.post-header p');
+console.log('작성자:', username.textContent);
 
-caption.textContent = "새벽 러닝 🌙";
-console.log("바꾼 뒤:", caption.textContent);
+caption.textContent = '새벽 러닝 🌙';
+username.textContent = 'soongu';
+
