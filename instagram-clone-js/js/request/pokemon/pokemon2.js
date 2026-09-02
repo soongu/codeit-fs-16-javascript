87e49a67143fb5cbdd6c2ca39c98d5309aaf0fba
soongu <naong526@naver.com>
2026-09-02 15:00:21 +0900

    모달 기능 개선: 포켓몬 상세정보를 비동기로 불러오고 타입 아이콘 추가

--- instagram-clone-js/js/request/pokemon/pokemon2.js
+++ instagram-clone-js/js/request/pokemon/pokemon2.js
@@ -1,6 +1,85 @@
 // ===== 상수 선언 ==== //
 const DISABLED = 'disabled';
 
+const typeImages = [
+  {
+    name: 'normal',
+    url: 'https://i.namu.wiki/i/OzwwiCpLEnz-zMSYWxCLyvZ90yPuyyaURmeJFU3aX4C7BEiAWTtX33jEBSwyVyt7KLpGwvkbC-HrPdtNomOuSw.svg',
+  },
+  {
+    name: 'fighting',
+    url: 'https://i.namu.wiki/i/w1xrIispBygNlATHMZo5-ZEFjWFAgJ6-oYTWbirc92co2jYPthtgSLS3AIadhu6vay0V5qtK5wyGWgQzuIQ84Q.svg',
+  },
+  {
+    name: 'flying',
+    url: 'https://i.namu.wiki/i/CQLOW9IDPvfxuIIppTGYBa3y2TsEuk5FybazRwqeuaFgF9FNHRZHUkWa8tsjL7B2FFdD_9MatdgQQyH_KBKa4w.svg',
+  },
+  {
+    name: 'poison',
+    url: 'https://i.namu.wiki/i/c87iqpANL6ODCg9-z4pNORiIpv5_CcvQZY83s-5vmNBdk0EvJBhk0An4iEqCagxcpLgETPlIFM6MWHpJ49wNWQ.svg',
+  },
+  {
+    name: 'ground',
+    url: 'https://i.namu.wiki/i/_1mjTC4i4eo1U8dXQVfn76wJ_8c6XZ8YBR712UelnIIaeDYNQx4sZ0GM9sw9zO0-QZwDhfcYPgA3bCEDVOQdhA.svg',
+  },
+  {
+    name: 'rock',
+    url: 'https://i.namu.wiki/i/8m5bA5hAR8wz07X0yHh3goxFP4Hq9fTqVnN1fGUa1SjptLefdMWZrCnxEbXpMuTGVGCyZ56bkHy2-K1qL987eA.svg',
+  },
+  {
+    name: 'bug',
+    url: 'https://i.namu.wiki/i/jHoVDx4jP_ibO6b4MYWC4uodKB7nBGRbUcMApoYKjPEt3ZP_rYzy__nAlUTRiR7v3Ug0WEo-m6XekRPGbopiSw.svg',
+  },
+  {
+    name: 'ghost',
+    url: 'https://i.namu.wiki/i/mU0K2HVDEo3NWXUwJkDAdxw852nUBuUun8DJmvaRNrycYIUa9P7i3QrLvpvP9kr2GoH4zE6GeRRvUoDKfXrb2g.svg',
+  },
+  {
+    name: 'steel',
+    url: 'https://i.namu.wiki/i/6COggx_E1MEEOQY57cb18Wbi2oGbp0jZn7BYfh72rQ8K13X5-93sCohZbpxqyuzYypMGsCWOpjzdLUrQqN3UwA.svg',
+  },
+  {
+    name: 'fire',
+    url: 'https://i.namu.wiki/i/3Rd_IL-T3OpLiZf3NFjqGmHcLi8TVyhXc4J7aiPzBUkDLgGiD3tziNdNKzSLHqdqHbYAAPfUHOw8sY2UTp3foA.svg',
+  },
+  {
+    name: 'water',
+    url: 'https://i.namu.wiki/i/aIjVcS_Rifmg45S1epta4n9kfoYodbMVvFEenybo5Xx3NNWGBUDAK5951yYryu6X8gE7zvFeDyfe8-LFFZ0nwA.svg',
+  },
+  {
+    name: 'grass',
+    url: 'https://i.namu.wiki/i/vl2AcEdMbkTBnGaxPWdhEhDO75sn_o8oZp_2BYyzjtnUFQWCFYk5WjVy1V2ePPzC2MQKsORU6jhiM2zJnMPDjw.svg',
+  },
+  {
+    name: 'electric',
+    url: 'https://i.namu.wiki/i/aIGMuCdw1p7AG4NDGxWfHvcu_QJgHwiXhhx-5OYTL0tQGDWkNOZcS77z_lfd-Qr9aD3gKGJCNCruw-9HBJ0NXkOyrkluoZGfTjxyWpbsNwnJxPd1ma4ByWN6Ur3Y4Q0CrqsJ-xTj3eKAzYgF-pJrwA.svg',
+  },
+  {
+    name: 'psychic',
+    url: 'https://i.namu.wiki/i/0Gd4OOht1-sgSyAyTGTAztx9X0xviiokovnUkok2F_3wGTiO81Aw66KeTf9-4MVFbxn7IbhJY1Ra8UrUWJFPOQ.svg',
+  },
+  {
+    name: 'ice',
+    url: 'https://i.namu.wiki/i/mW1sfdpreP-IWq12WE8AnamlNHAU-kCLeUODy6bAB-mgpzDqYMtcjiw-OJRzjSEor-eVc7qFG54f2WGoEOjP_A.svg',
+  },
+  {
+    name: 'dragon',
+    url: 'https://i.namu.wiki/i/WmIOiZtmmBqIvahkVaOI2C5Nmn484bhPOO118dllYEpBh2Vqglzg5647g3mjrLtoG5qRlpTcMpI0spOkIpTSdw.svg',
+  },
+  {
+    name: 'dark',
+    url: 'https://i.namu.wiki/i/eDyMcr395uKyNxT5v3b1fAdwZTuxY2c1GlRKT2gW4OmrKNV6JBvP8-OrOw9_sLWNF7gP-pm-7balFwZi6crx5A.svg',
+  },
+  {
+    name: 'fairy',
+    url: 'https://i.namu.wiki/i/jI-idDQjmCCHxnvdcvWKLl5Yj428W4PguwleARun7RqjY5Me7ij_W8TB3GKYLKZSX68ccACnmG_DFXBJEbXvNw.svg',
+  },
+  {
+    name: 'stellar',
+    url: 'https://i.namu.wiki/i/hf2gRypYf0w0oCk6p9LVr0E1rWjIMVQTdAMZht3g8A6ecvR7v8hyoZY3JXP-Sx-1nHSY7biJXZsAMdxrayNOyg.webp',
+  },
+];
+
 // ===== DOM 캐싱 ===== //
 const pokeContainer = document.querySelector('.pokemon-container');
 const prevBtn = document.querySelector('#prev');
@@ -8,6 +87,15 @@ const nextBtn = document.querySelector('#next');
 const loadingSpinner = document.querySelector('#loading');
 const modal = document.querySelector('#modal');
 const overlay = document.querySelector('#overlay');
+const modalCloseBtn = document.querySelector('#closeModal');
+
+const modalContent = document.querySelector('#modal-content');
+const modalTitle = document.querySelector('#modalTitle');
+const modalImage = document.querySelector('#modalImage');
+const modalDetails = document.querySelector('#modalDetails');
+const modalTypes = document.querySelector('#modalTypes');
+
+const modalSkeleton = document.querySelector('#modal-skeleton');
 
 // ===== 전역 변수 선언 =====//
 const url = 'https://pokeapi.co/api/v2/pokemon';
@@ -69,12 +157,58 @@ async function getPokemon() {
 }
 
 // 모달 열기 함수
-const openModal = () => {
+const openModal = async (pokemonName) => {
   modal.style.display = 'flex';
   overlay.style.display = 'flex';
+  // 모달이 열릴 때 뒷배경 스크롤링 방지
+  document.body.style.overflow = 'hidden';
+
+  // 명시적으로 스켈레톤을 끄고 컨텐츠를 렌더링
+  modalSkeleton.style.display = 'none';
+  modalContent.style.display = 'flex';
+
+  // 지금 클릭한 포켓몬이 누구?
+  // console.log('클릭한 포켓몬: ', pokemonName);
+  // 포켓몬 상세 데이터를 fetching
+  const res = await fetch(`${url}/${pokemonName}`);
+  const pokemonData = await res.json();
+  const { name, height, weight, base_experience, types, sprites } = pokemonData;
+  
+  // 포켓몬 데이터를 렌더링
+  modalTitle.textContent = name;
+  modalImage.src = sprites.front_default;
+  modalDetails.textContent = `키: ${height} | 무게: ${weight} | 경험치: ${base_experience}`;
+
+  // 타입 데이터 삽입
+  const typeNames = types.map(t => t.type.name);
+
+  modalTypes.innerHTML = '';
+  for (const typeName of typeNames) {
+    const liTag = document.createElement('li');
+
+    // 이 타입명에 해당하는 아이콘 url을 탐색하기
+    const typeImageUrl = typeImages.find(ti => ti.name === typeName)?.url;
+
+    liTag.innerHTML = `<img src="${typeImageUrl}">`;
+
+    modalTypes.append(liTag);
+  }
+
+};
+
+// 모달 닫기 함수
+const closeModal = () => {
+  modal.style.display = 'none';
+  overlay.style.display = 'none';
+  // 모달이 열릴 때 뒷배경 스크롤링 방지 해제
+  document.body.style.overflow = '';
 };
 
 // ===== 이벤트 핸들러 바인딩 ===== //
+
+// 모달 닫기 바인딩
+modalCloseBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 // 다음 버튼 클릭시
 nextBtn.addEventListener('click', event => { 
@@ -112,10 +246,12 @@ prevBtn.addEventListener('click', event => {
 
 // 포켓몬을 클릭하면 실행할 이벤트
 pokeContainer.addEventListener('click', event => { 
-  if (!event.target.closest('.pokemon')) return;
+  const pokeDiv = event.target.closest('.pokemon');
+
+  if (!pokeDiv) return;
 
   // 모달을 열어주기
-  openModal();
+  openModal(pokeDiv.querySelector('img').alt);
 });
 
 
